85b197ebb9943ca8c5231d4eca5386540840855a
mayu <2296991161@qq.com>
2019-07-19 20:08:28 +0800

    底部滚动部分

--- lib/common/my_constants.dart
+++ lib/common/my_constants.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class MyColors{
   static const Color themeColor = Color(0xFF597EF7);
+  static const Color mainBgColor = Color.fromRGBO(245, 247, 251, 1.0);
+
 }
--- lib/views/home_home.dart
+++ lib/views/home_home.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:app_model/views/home_topSwiper.dart';
-import 'package:app_model/common/my_constants.dart';
 import 'package:app_model/model/home/home_button_items.dart';
 import 'package:app_model/common/screen_util.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:app_model/widget/home/home_btnListWidget.dart';
 import 'package:app_model/widget/home/animation_headlines.dart';
+import 'package:app_model/common/my_constants.dart';
 
 class FirstPage extends StatefulWidget {
   @override
@@ -13,21 +13,69 @@ class FirstPage extends StatefulWidget {
 }
 
 
-class FirstPageState extends State<FirstPage> with AutomaticKeepAliveClientMixin{
+class FirstPageState extends State<FirstPage> with TickerProviderStateMixin, AutomaticKeepAliveClientMixin{
   @override
   bool get wantKeepAlive => true;
   List<HomeBtnItem> HomeBtnItems;
+  List _tabTitles = ['热门', '资讯', '电子'];
+  Size _sizeRed;
+
+  TabController _controller;
+  int _currentIndex = 0;
+
+  ScrollController _scrollViewController;
+  GlobalKey _keyFilter = GlobalKey();
+
+  void initData() async {
+//    List querys = await getHotSugs();
+    setState(() {
+//      _hotWords = querys;
+    });
+  }
+
+  _afterLayout(_) {
+    _getPositions('_keyFilter', _keyFilter);
+    _getSizes('_keyFilter', _keyFilter);
+  }
+
+  _getPositions(log, GlobalKey globalKey) {
+    RenderBox renderBoxRed = globalKey.currentContext.findRenderObject();
+    var positionRed = renderBoxRed.localToGlobal(Offset.zero);
+    print("POSITION of $log: $positionRed ");
+  }
+
+  _getSizes(log, GlobalKey globalKey) {
+    RenderBox renderBoxRed = globalKey.currentContext.findRenderObject();
+    _sizeRed = renderBoxRed.size;
+    setState(() {});
+    print("SIZE of $log: $_sizeRed");
+  }
 
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
+
     HomeBtnItems = HomeBtnList.fromJson(menueDataJson['items']).items;
-    print('duandian');
+
+    initData();
+    _controller = TabController(vsync: this, length: 3);
+    _controller.addListener(_handleTabSelection);
+
+    _scrollViewController = ScrollController(initialScrollOffset: 0.0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollViewController.dispose();
+    _controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
+//    super.build(context);
+
     var views = Column(
       children: <Widget>[
         SwiperPage(),
@@ -36,8 +84,73 @@ class FirstPageState extends State<FirstPage> with AutomaticKeepAliveClientMixin
       ],
     );
 
+    var body = NestedScrollView(
+      controller: _scrollViewController,
+      headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            pinned: true,
+            floating: true,
+            forceElevated: boxIsScrolled,
+            backgroundColor: MyColors.mainBgColor,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              background: Column(
+//                  key: _keyFilter,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[views],
+              ),
+            ),
+            expandedHeight: (_sizeRed == null ? 360 : _sizeRed.height) + 50.0,
+            bottom: PreferredSize(
+              preferredSize: Size(double.infinity, ScreenUtil.screenWidth / 3),
+              child: TabBar(
+                  controller: _controller,
+                  indicatorColor: Colors.transparent,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  tabs: _tabTitles.map((i) => Container(
+                    padding: const EdgeInsets.all(0),
+                    height: 44.0,
+                    child: new Tab(
+                      child: Row(
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(i),
+                              SizedBox(
+                                height: 3,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            width: 1,
+                            height: 30,
+                            color: Color(0xFFc9c9ca),
+                          )
+                        ],
+                      ),
+                    ),
+                  )).toList()
+                )
+            ),
+          )
+        ];
+      },
+      body: TabBarView(controller: _controller, children: [
+        _buildListView("aaa:"),
+        _buildListView("bbb:"),
+        _buildListView("ccc:"),
+      ])
+    );
+
     return Scaffold(
-        backgroundColor: Color.fromRGBO(245, 247, 251, 1.0),
+        backgroundColor: MyColors.mainBgColor,
         appBar: AppBar(
           title: new Text('标题'),
           actions: <Widget>[
@@ -51,17 +164,7 @@ class FirstPageState extends State<FirstPage> with AutomaticKeepAliveClientMixin
           ],
           centerTitle: true,
         ),
-        body: Column(
-          children: <Widget>[
-            Offstage(
-              offstage: false,
-              child: Container(
-                child: views,
-//                key: _keyFilter,
-              ),
-            ),
-          ],
-        )
+        body: body
     );
   }
 
@@ -105,27 +208,6 @@ class FirstPageState extends State<FirstPage> with AutomaticKeepAliveClientMixin
 
         ///
         itemCount: (HomeBtnItems.length / 5).toInt() + (HomeBtnItems.length % 5 > 0 ? 1 : 0),
-
-        /// 设置 new SwiperPagination() 展示默认分页指示器
-//        pagination: new SwiperPagination(
-//            alignment: Alignment.bottomCenter,
-//            builder: RectSwiperPaginationBuilder(
-//                color: Color(0xFFd3d7de),
-//                activeColor: Theme.of(context).primaryColor,
-//                size: Size(18, 3),
-//                activeSize: Size(18, 3),
-//                space: 0)
-//        ),
-
-        /// 设置 new SwiperControl() 展示默认分页按钮
-        // control: SwiperControl(),
-
-        /// 自动播放开关.
-//        autoplay: false,
-
-        /// 动画时间，单位是毫秒
-//        duration: 300,
-
         /// 当用户点击某个轮播的时候调用
         onTap: (index) {
           print('点击了第$index个');
@@ -136,16 +218,32 @@ class FirstPageState extends State<FirstPage> with AutomaticKeepAliveClientMixin
       ),
     );
   }
-}
 
-Widget _buildAnimationLineWidget() {
-  return Container(
-    height: 42,
-    margin: const EdgeInsets.all(5),
-    decoration: new BoxDecoration(
-      borderRadius: BorderRadius.circular(21),
-      color: Colors.white,
-    ),
-    child: AnimationHeadlinesWidget(),
-  );
+  Widget _buildAnimationLineWidget() {
+    return Container(
+      height: 42,
+      margin: const EdgeInsets.all(5),
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.circular(21),
+        color: Colors.white,
+      ),
+      child: AnimationHeadlinesWidget(),
+    );
+  }
+
+  _handleTabSelection() {
+    print('_handleTabSelection:${_controller.index}');
+    setState(() {
+      _currentIndex = _controller.index;
+    });
+  }
+
+  Widget _buildListView(String s){
+    return ListView.separated(
+        itemCount: 20,
+        separatorBuilder: (BuildContext context, int index) =>Divider(color: Colors.grey,height: 1,),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(color: Colors.white, child: ListTile(title: Text("$s第$index 个条目")));
+        });
+  }
 }
